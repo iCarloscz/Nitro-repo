2baa04c8f98bfb94ecadb0de1604b2f09c9eb78f
iCarloscz <carlos.cardenasg1@gmail.com>
2019-12-11 14:01:53 -0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import {Animated} from "react-animated-css";
+import Header from "./components/Navigation/Header/Header";
+import Footer from "./components/Footer.js";
+import "./Footer.css";
+function App() {
+  console.log(process.env.REACT_APP_API_URL);
+
+  return (
+    <React.Fragment>
+      <Header />
+      <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
+        <div className="App">
+          <h1>React App</h1>
+          <Button variant="danger">Hello</Button>
+        </div>
+      </Animated>
+      <Footer/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import {BrowserRouter} from 'react-router-dom';
+
+require('dotenv').config();
+
+axios.defaults.baseURL = 'URL';
+
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
